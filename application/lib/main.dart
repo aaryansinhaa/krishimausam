14df72a6161d690a887e90e76fe6d2af460513d4
Aaryan Kumar Sinha <138802020+aaryansinhaa@users.noreply.github.com>
2024-05-04 20:26:25 +0530

    ..

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Scaffold(
+        home: const Scaffold(
           body: Center(child: Text("Init!")),
         ));
   }
