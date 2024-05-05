3e5b4a8e2993cf2f9fdaf49e8aa4c0bd65086194
Aaryan Kumar Sinha <138802020+aaryansinhaa@users.noreply.github.com>
2024-05-05 23:36:33 +0530

    made the loading screen

--- application/lib/screens/loading.dart
+++ application/lib/screens/loading.dart
@@ -1,7 +1,10 @@
 import 'package:application/screens/home.dart';
 import 'package:application/utility/helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -53,14 +56,82 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Container(
-        child: Shimmer.fromColors(
-            child: Text("Loading"),
-            baseColor: Colors.grey.shade300,
-            highlightColor: Colors.grey.shade100),
-      )),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/bgimg.png"),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Column(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Krishi Mausam",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 35,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "A weather forecast App",
+                            style: GoogleFonts.poppins(
+                                fontSize: 15, color: Colors.yellow.shade300),
+                          ),
+                          Image.asset("assets/images/logo.png")
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Shimmer.fromColors(
+                        child: Text(
+                          "Loading",
+                          style: GoogleFonts.poppins(fontSize: 50),
+                        ),
+                        baseColor: Colors.white,
+                        highlightColor: Color.fromARGB(255, 233, 222, 123),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              RichText(
+                  textAlign: TextAlign.end,
+                  text: TextSpan(children: [
+                    TextSpan(
+                        text: "Made with ",
+                        style: TextStyle(color: Colors.black, fontSize: 15)),
+                    WidgetSpan(
+                      child: Icon(
+                        CupertinoIcons.heart,
+                        size: 15,
+                      ),
+                    ),
+                    TextSpan(
+                        text: " by Aaryan",
+                        style: GoogleFonts.dancingScript(
+                            color: Colors.yellow, fontSize: 15))
+                  ]))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
